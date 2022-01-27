0cab6ae34b5cff119444f8c4ea5c4d69159982bc
Paulo Oliveira <paulo@ninito.me>
2022-01-27 01:50:50 -0300

    Add AssetRenameOperation struct

---
+++ Editor/AssetRenamer/AssetRenameOperation.cs
@@ -0,0 +1,38 @@
+﻿using Ninito.UnityProjectLinter.Editor.Linter;
+using Ninito.UnityProjectLinter.Editor.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace Ninito.UnityProjectLinter.Editor.AssetRenamer
+{
+	/// <summary>
+	/// A struct that describes the operation of renaming a single asset.
+	/// </summary>
+	public struct AssetRenameOperation
+	{
+		#region Fields
+
+		public Object Asset;
+		public string NewName;
+		public string SuggestedName;
+
+		#endregion
+
+		#region Properties
+
+		public string CurrentName => Asset.name;
+
+		#endregion
+
+		#region Constructors
+
+		public AssetRenameOperation(string assetPath)
+		{
+			Asset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+			NewName = AssetNameUtility.GetAssetNameByPath(assetPath);
+			SuggestedName = AssetNameLinter.GetSuggestedNameFor(assetPath);
+		}
+
+		#endregion
+	}
+}
